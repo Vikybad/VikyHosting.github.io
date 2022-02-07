138b6982c4087d609d82036cfa1ed3966d496e26
Viky <0.8vikrambadesara@gmail.com>
2022-02-07 19:00:23 +0530

    Create day2.js

---
+++ day2.js
@@ -0,0 +1,14 @@
+function Viky()
+{
+alert("Hii\nIt's Viky here....");
+}
+
+function Pooja()
+{
+alert("Hii\nIt's Pooja Bishu here....");
+}
+
+function Deepali()
+{
+alert("Hii\nIt's Deepali Garg here....");
+}
